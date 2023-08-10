971ac585c658b36385f729644983666c4b1173ab
Hak Ima <hakimaibrahimi786@gmail.com>
2023-08-10 13:58:49 -0400

    last

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Quiz from './Quiz';
-import bImage from './img/b.jpeg';
-import mnImage from './img/mn.jpeg';
-import uImage from './img/u.jpeg';
-import hImage from './img/h.jpeg';
-import cImage from './img/c.jpeg';
-import AImage from './img/A.jpg';
-import cmage from './img/c.jpg';
+import q2 from './img/q2.jpg';
+import q from './img/q.jpg';
+import y2 from './img/y2.jpg';
+import y from './img/y.jpg';
+import c2 from './img/c2.jpg';
+import c from './img/c.jpg';
+import n2 from './img/n2.jpg';
+import n from './img/n.jpg';
+import a2 from './img/a.jpg';
+import a from './img/a.jpg';
+import e2 from './img/e2.jpg';
+import e from './img/e.jpg';
+import i2 from './img/i2.jpg';
+import i from './img/i.jpg';
+import k2 from './img/k2.jpg';
+import k from './img/k.jpg';
+import o2 from './img/o2.jpg';
+import o from './img/o.jpg';
+import z2 from './img/z2.jpg';
+import z from './img/z.jpg';
+
 function App() {
-  const images = [hImage,cmage, AImage,cImage,hImage,mnImage, cImage, bImage,mnImage, uImage, bImage, AImage, cmage,uImage];
+  
+  const images = [z, o, k, i, e, a, n, c, z, y, q, z2, o2, k2, i2, e2, a2, n2,c2, y2, q2,z, o, k, i, e, a, n, c, y, q, z2, o2, k2, i2, e2, a2, n2, c2, y2, q2,i];
   const [flippedIndexes, setFlippedIndexes] = useState([]);
   const [matchedIndexes, setMatchedIndexes] = useState([]);
   const [showQuiz, setShowQuiz] = useState(true); 
@@ -41,7 +56,7 @@ function App() {
       {/* <button onClick={toggleView}>Toggle View</button> */}
       <div className="image-container">
         {showQuiz ? (
-          <Quiz CardView={CardView}/>
+          <Quiz CardView={CardView} />
         ) : (
           images.map((image, index) => (
             <div
@@ -55,7 +70,9 @@ function App() {
         )}
       </div>
     </div>
+    
   );
+
 }
 
 export default App;
--- src/QuizData.js
+++ src/QuizData.js
@@ -11,8 +11,8 @@
         answer: 1
     },
     {
-        question: "Which planet is closest to the Sun?",
-        options: ["Mars", "Venus", "Mercury", "Jupiter"],
+        question: "What is the smallest country in the world by land area?",
+        options: ["Brazil", "Monaco", " Vatican City", "China"],
         answer: 3
     },
     {
@@ -31,9 +31,23 @@
         answer: 2
     },
     {
-        question: "What is the largest planet in our solar system?",
-        options: ["Jupiter", "Venus", "Mars", "Saturn"],
+        question: "How many people are estimated to be living in the world?",
+        options: ["7.8 billion", "8 billion", "6 billion", "7.5 billion"],
         answer: 1
     },
-   
+    {
+        question: "How many languages are estimated to exist in the world?",
+        options: ["7,000", "8,000", "6,000", "500"],
+        answer: 1
+    },
+    {
+        question: "The native language commonly spoken in Colombia is?",
+        options: ["Spanish",  "Portuguese", "Quechua", "Guarani"],
+        answer: 1
+    },
+    {
+        question: "Which of the following animals holds cultural significance in India?",
+        options: ["Peacock",  "Lion", "Elephant", "Kangaroo"],
+        answer: 1
+    },
 ]    
